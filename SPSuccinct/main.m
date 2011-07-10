1e8cb8c9dfc2a9cb11df9405f733bd1412445f91
Joachim Bengtsson <nevyn@spotify.com>
2011-07-10 16:15:01 +0200

    Initial Commit

---
+++ SPSuccinct/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  SPSuccinct
+//
+//  Created by Joachim Bengtsson on 2011-07-10.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
