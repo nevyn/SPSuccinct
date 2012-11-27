509d1718cf996da50b97a983218cfd514aff4e4e
Joachim Bengtsson <nevyn@spotify.com>
2012-11-27 17:01:31 +0100

    Setup unit testing

---
+++ succinctTests/succinctTests.h
@@ -0,0 +1,13 @@
+//
+//  succinctTests.h
+//  succinctTests
+//
+//  Created by Joachim Bengtsson on 2012-11-27.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface succinctTests : SenTestCase
+
+@end
